a51935cf130cb12853442a0a0bbc4c51616cc481
DP <durga66352@gmail.com>
2026-01-29 12:01:33 +0530

    changed

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+PERSON1=durga 
+PERSON2=bunny
+echo "$PERSON1 : hello who are you?"
+echo "$PERSON2 : my name is $PERSON2,what is your name ?"
+echo "$PERSON1 : my name is $PERSON1. what are yoou doing."
