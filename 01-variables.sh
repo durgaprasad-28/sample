81bbfce16d7bf3468cc63b95d9d1f6b0393a99d8
DP <durga66352@gmail.com>
2026-01-29 11:52:28 +0530

    changed

---
+++ 01-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+durga : hello who are you?
+bunny : my name is bunny,what is your name ?
+durga : my name is durga. what are yoou doing.
+bunny : nothing.i am learning abiut devops/what about u
+durga : same here. learnig about devops bunny
+bunny :  nice durga. where are from learning durga
