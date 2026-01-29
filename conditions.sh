3b506583b9fce55a151dee9188883adafcc6e246
DP <durga66352@gmail.com>
2026-01-29 13:51:43 +0530

    changed

---
+++ conditions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+NUM1=$1
+NUM2=$2
+NUM3=$NUM1+$NUM2
+if [ $NUM3 -gt 100 ]; then
+echo "the number is greater than 100"
+elif [ $NUM3 -lt 100 ]; then
+echo "the number is lesser than 100"
+elif [ $NUM3 -eq 100 ]; then
+echo "the number is equal to 100"
+elsee [ $NUM3 -ne 100 ]; then
+echo "the number is not equal to 100"
+fi
