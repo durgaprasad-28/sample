641d82a4f88b823b484ad8251f8d4d973bca1d57
DP <durga66352@gmail.com>
2026-01-29 13:31:33 +0530

    changed

---
+++ datatypes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+NUM1=100
+NUM2=250
+NUM3=$(($NUM1=$NUM2))
+echo "the sum is $NUM3"
+PLAYERS=("messi" "neymar" "mbappe" "ronaldo")
+echo "${PLAYERS[@]}"
+echo "${PLAYERS[0]}"
+echo "${PLAYERS[1]}"
+echo "${PLAYERS[2]}"
+echo "${PLAYERS[3]}"
