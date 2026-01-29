4ee9e3bb50bb1d31002947a91dac5a47f67aa940
DP <durga66352@gmail.com>
2026-01-29 11:58:04 +0530

    changed

--- 01-variables.sh
+++ 01-variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-durga : hello who are you?
-bunny : my name is bunny,what is your name ?
-durga : my name is durga. what are yoou doing.
-bunny : nothing.i am learning abiut devops/what about u
-durga : same here. learnig about devops bunny
-bunny :  nice durga. where are from learning durga
+echo "durga : hello who are you?"
+echo "bunny : my name is bunny,what is your name ?"
+echo "durga : my name is durga. what are yoou doing."
+echo "bunny : nothing.i am learning abiut devops/what about u"
+echo "durga : same here. learnig about devops bunny"
+echo "bunny :  nice durga. where are from learning durga"
