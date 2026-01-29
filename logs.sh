fec99810460978baf05555af2d94f5874af1775a
DP <durga66352@gmail.com>
2026-01-29 21:26:32 +0530

    changed

--- logs.sh
+++ logs.sh
@@ -3,7 +3,7 @@ ID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/$0.log"
 if [ $ID -gt 0 ]; then
-echo "please acces to the root user to install packages- INSTALLATION FAILED"
+echo "please acces to the root user to install packages- INSTALLATION FAILED" | tee $LOGS_FILE
 exit 1
 fi
 mkdir -p $LOGS_FOLDER
@@ -17,9 +17,9 @@ echo "$3"
 fi
 }
 dnf install nginx -y &>> $LOGS_FILE
-VALIDATE $? "check the command" "SUCCESSFULLY INSTALLED NGINX"
+VALIDATE $? "check the command" "SUCCESSFULLY INSTALLED NGINX" | tee $LOGS_FILE
 dnf install mysql -y &>> $LOGS_FILE
-VALIDATE $? "check the command" "SUCCESSFULLY INSTALLED MYSQL"
+VALIDATE $? "check the command" "SUCCESSFULLY INSTALLED MYSQL" | tee $LOGS_FILE
 dnf install nodejs -y &>> $LOGS_FILE
-VALIDATE $? "check the command" "SUCCESSFULLY INSTALLED NODEJS"
+VALIDATE $? "check the command" "SUCCESSFULLY INSTALLED NODEJS" | tee $LOGS_FILE
 
