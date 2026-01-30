d3a5cf40fc61fc6284ed9dec824dd93375686348
DP <durga66352@gmail.com>
2026-01-30 13:55:21 +0530

    changed

--- trap.sh
+++ trap.sh
@@ -14,21 +14,12 @@ echo -e " $Y please acces to the root user to install packages $N - $R INSTALLAT
 exit 1
 fi
 mkdir -p $LOGS_FOLDER
-VALIDATE() {
-if [ $1 -ne 0 ]; then
-echo "$2" | tee -a $LOGS_FILE
-exit 1
-else
-echo "$3" | tee -a $LOGS_FILE
-fi
-}
 for package in $@
 do
    dnf list installed $package &>> $LOGS_FILE
    if [ $? -ne 0 ]; then
     echo "installing $package"
   dnf install $package -y &>> $LOGS_FILE
-  VALIDATE $? "check the command" "SUCCESSFULLY INSTALLED $package"
   else
    echo -e " $B already installed $package,ignore it" | tee -a $LOGS_FILE
    fi 
