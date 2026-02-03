1abafd2db92a1ad5001b2534e646d25a672a41a0
DP <durga66352@gmail.com>
2026-02-03 12:16:43 +0530

    changed

--- script01.sh
+++ script01.sh
@@ -2,3 +2,5 @@ person1=durgaprasad
 favouritefootballer=messi
 echo "name:$person1"
 echo "favourite footballer is $favouritefootballer"
+echo "pid is $$"
+
--- script02.sh
+++ script02.sh
@@ -2,4 +2,5 @@ person1=dsp
 favouritefootballer=ronaldo
 echo "name:$person1"
 echo "favourite footballer is $favouritefootballer"
-sh script02.sh
+echo "pid is $$"
+sh script01.sh
