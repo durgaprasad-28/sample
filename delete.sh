76cf16a7f055e475b976004ff0f8727402791118
DP <durga66352@gmail.com>
2026-02-04 23:13:32 +0530

    changed

--- delete.sh
+++ delete.sh
@@ -12,7 +12,9 @@ else
 echo -e  "$G the folder exists, waiting for ur command boss $N"
 fi
 your_folder=$(find $logs_folder -type f -name "*.log" -mtime +14)
-echo "your required files are $B $your_folder $N"
+echo "your required files to delete  $your_folder "
 while IFS= read -r files ; do
 echo "reading $files"
+rm -f $files
+echo "files are deleted ,they are : $files"
 done <<< $your_folder
