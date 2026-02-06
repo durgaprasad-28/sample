1f13ae290d17a6630ef84e7049d6a680ee29c141
DP <durga66352@gmail.com>
2026-02-06 22:36:33 +0530

    changed

--- disk.sh
+++ disk.sh
@@ -2,7 +2,7 @@
 limit=3
 usage=$(df -hT | grep -v Filesystem | awk '{print $6}' | cut -d "%" -f1)
 partition_name=$(df -hT | grep -v Filesystem | awk '{print $7}')
-while IFS=read -r line 
+while IFS= read -r line ;
 do
 if [ echo $line -gt $limit ]; then
 echo "disk usage is high,they are:-echo $line% - $partition_name"
