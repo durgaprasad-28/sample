31c97505df3c6eb5b047c26eb4cd2415042fd34b
DP <durga66352@gmail.com>
2026-02-07 08:41:56 +0530

    changed

--- mail.sh
+++ mail.sh
@@ -3,10 +3,10 @@ TO_ADDRESS=$1
 SUBJECT=$2
 team_name=$3
 alert=$4
-ip_address=$5
-message=$6
+private_ip_address=$5
+message_body=$6
 EMAIL_BODY=$(sed -e "s/team_name/$3/g" -e "s/ALERT/$4/g" -e "s/ip_address/$5/g" -e "s/message/$body/g" disk.html )
-body=$(echo $message | sed -e "s/'/'\\\\''/g")
+body=$(echo $message_body | sed -e "s/'/'\\\\''/g")
 {
 echo "To: $TO_ADDRESS"
 echo "Subject: $SUBJECT"
