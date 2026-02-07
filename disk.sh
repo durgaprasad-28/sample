c78fcf1a1d76e1ff28afdda62673a55e29d0f86a
DP <durga66352@gmail.com>
2026-02-07 07:19:37 +0530

    changed

--- disk.sh
+++ disk.sh
@@ -6,12 +6,13 @@ B="\e[34m"
 N="\e[0m"
 limit=3
 disk_info=$(df -hT | grep -v Filesystem)
+echo -e  "$R alert:high disk usage$N"
 while IFS= read -r line ;
 do
  usage=$(echo "$line" | awk '{print $6}' | cut -d "%" -f1)
  partition_name=$(echo "$line" | awk '{print $7}')
 if [ $usage -gt $limit ] ; then
-message+="disk is high,they are:-$partition_name-$usage% \n"
+message+="$partition_name-$usage% \n"
 fi
 done <<< $disk_info
 echo -e "$message"
