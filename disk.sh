add64e21b763030dd5670966373052be3788dd64
DP <durga66352@gmail.com>
2026-02-07 07:16:20 +0530

    changed

--- disk.sh
+++ disk.sh
@@ -6,13 +6,12 @@ B="\e[34m"
 N="\e[0m"
 limit=3
 disk_info=$(df -hT | grep -v Filesystem)
-message=""
 while IFS= read -r line ;
 do
  usage=$(echo "$line" | awk '{print $6}' | cut -d "%" -f1)
  partition_name=$(echo "$line" | awk '{print $7}')
 if [ $usage -gt $limit ] ; then
-$message+=disk  is high,they are:-$partition_name-$usage% \n
+message+=disk  is high,they are:-$partition_name-$usage% \n
 fi
 done <<< $disk_info
 echo -e "$message"
